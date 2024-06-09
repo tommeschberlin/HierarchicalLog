3ba8bc89a676f0d4e8ee7fb8386a0ea04b40ef4b
Thomas Schwarze <thomas.schwarze@tommesch.de>
2024-06-09 20:26:26 +0200

    Tests for log hierarchy stage

---
+++ hlog/hlogTest.py
@@ -0,0 +1,84 @@
+import logging.handlers
+import os
+import unittest
+import logging
+import re
+
+from hlog import *
+
+class TestHierarchicalLog(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.workDir = os.path.join( 'c:/', 'tmp' )
+        if not os.path.exists( self.workDir ):
+            raise Exception( 'Error', 'No workDir \"%s\" found!' % self.workDir )
+    
+    def setUp(self):
+        self.logFile = os.path.join( self.workDir, 'test.log' )
+        if os.path.isfile(self.logFile):
+            os.remove(self.logFile)
+        #self.logToConsole = LogToConsole( globalLog )
+        self.logger = logging.getLogger('test')
+        self.logger.setLevel(logging.DEBUG)
+        self.fileHandler = logging.FileHandler(self.logFile)
+        # fileHandler.setFormatter(logFormatter)
+        self.logger.addHandler(self.fileHandler)
+
+    def logFileContent(self):
+        with open(self.logFile) as f:
+            return f.readlines()
+
+    # Test 
+    #@unittest.skip("skipped temporarily")
+    def test_basic(self):
+        self.logger.info('Started')
+        self.logger.info('Finished')
+        self.fileHandler.close()
+        content = '\n'.join( self.logFileContent() )
+        self.assertTrue( re.search("Started", content ), "Check Started" )
+        self.assertTrue( re.search("Finished", content ), "Check Finished" )
+
+    # Test if, hierarchy stage can be set in python logging system
+    # @unittest.skip("skipped temporarily")
+    def test_hierarchyHandling(self):
+        initLogHierarchy(self.logger)
+        records = []
+
+        class recordingHandler( logging.Handler ):
+            def __init__(self )-> None:
+                logging.Handler.__init__(self=self)
+            
+            def emit(self, record)->None:
+                records.append( record )
+
+        self.logger.addHandler(recordingHandler())
+
+        self.logger.info('Started')
+
+        def function():
+            self.logger.info('Function ist doing something')
+        
+        with IncreasedLogHierarchyStage("Function hier", self.logger):
+            function()
+
+        self.logger.info('Finished')
+
+        self.assertEqual( records[0].hierarchyStage, 0 , "Check Hierarchy stage" )
+        self.assertEqual( records[1].hierarchyStage, 0 , "Check Hierarchy stage" )
+        self.assertEqual( records[2].hierarchyStage, 1 , "Check Hierarchy stage" )
+        self.assertEqual( records[3].hierarchyStage, 0 , "Check Hierarchy stage" )
+
+
+
+
+
+    
+
+
+#def function1():
+#    stage = increaseHierarchyStage( "Function 1" )
+#log( LogLevel.INFO, "Bla")
+
+
+if __name__ == '__main__':
+    unittest.main()
